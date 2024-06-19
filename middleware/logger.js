30099661e4c9ecec66d5849c623e03f2de99e6f9
jjosephd <jjosephd804@gmail.com>
2024-06-19 09:53:38 -0400

    Morgan dev logger middleware added

--- middleware/logger.js
+++ middleware/logger.js
@@ -11,27 +11,10 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-const express = require('express');
-const dotenv = require('dotenv');
-const morgan = require('morgan');
+//@desc     Logging the HTTP method, protocol, host, and URL of an incoming request (req) to the console.
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.protocol}://${req.get('host')}${req.url}`);
+  next();
+};
 
-//  Route Files
-const bootcamps = require('./routes/bootcamps');
-
-// Load env vars
-dotenv.config({ path: './config/config.env' });
-
-const app = express();
-
-//Mount dev logger middleware
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
-}
-//Mount Routes
-app.use('/api/v1/bootcamps', bootcamps);
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () =>
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
-);
+module.exports = logger;
