165e2f9dc52529690c8568912894265bb48e71d7
jjosephd <jjosephd804@gmail.com>
2024-06-18 21:29:44 -0400

    Created controller methods to support router HTTP methods

---
+++ controllers/bootcamps.js
@@ -0,0 +1,50 @@
+// Copyright 2024 Jordan Daniel
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//@desc         Get all bootcamps
+//@route        GET /api/v1/bootcamps
+//@access       Public
+
+exports.getBootcamps = (req, res, next) => {
+  res.status(200).json({ success: true, msg: 'Show All Bootcamps' });
+};
+
+//@desc         Get single bootcamp
+//@route        GET /api/v1/bootcamps/:id
+//@access       Public
+
+exports.getBootcamp = (req, res, next) => {
+  res.status(200).json({ success: true, msg: `Access ${req.params.id}` });
+};
+
+//@desc         Create bootcamp entry
+//@route        GET /api/v1/bootcamps/:id
+//@access       Private
+exports.createBootcamp = (req, res, next) => {
+  res.status(200).json({ success: true, msg: 'Create New Bootcamp Entry' });
+};
+
+//@desc         Update single bootcamps
+//@route        PUT /api/v1/bootcamps/:id
+//@access       Private
+exports.updateBootcamp = (req, res, next) => {
+  res.status(200).json({ success: true, msg: `Update ${req.params.id}` });
+};
+
+//@desc         Delete single bootcamps
+//@route        DELETE /api/v1/bootcamps/:id
+//@access       Private
+exports.deleteBootcamp = (req, res, next) => {
+  res.status(200).json({ success: true, msg: `Delete ${req.params.id}` });
+};
--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -14,21 +14,23 @@
 
 const express = require('express');
 const router = express.Router();
-//Route Files
-router.get('/', (req, res) =>
-  res.status(200).json({ success: true, msg: 'Show All Bootcamps' })
-);
-router.get('/:id', (req, res) =>
-  res.status(200).json({ success: true, msg: `Access ${req.params.id}` })
-);
-router.post('/', (req, res) =>
-  res.status(200).json({ success: true, msg: 'Create New Bootcamp Entry' })
-);
-router.put('/:id', (req, res) =>
-  res.status(200).json({ success: true, msg: `Update ${req.params.id}` })
-);
-router.delete('/:id', (req, res) =>
-  res.status(200).json({ success: true, msg: `Delete ${req.params.id}` })
-);
+const {
+  getBootcamps,
+  getBootcamp,
+  createBootcamp,
+  updateBootcamp,
+  deleteBootcamp,
+} = require('../controllers/bootcamps');
+
+//  Root Path Route Files
+
+router.route('/').get(getBootcamps).post(createBootcamp);
+
+//  Route Files By Params
+router
+  .route('/:id')
+  .get(getBootcamp)
+  .put(updateBootcamp)
+  .delete(deleteBootcamp);
 
 module.exports = router;
